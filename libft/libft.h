8d3620c311896c251ed7b0ab299a664eaf055124
Maria del pilar Mora segura <mmora-se@c1r15s5.42madrid.com>
2021-05-02 16:10:23 +0200

    Subida 02-05-2021

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: mmora-se <mmora-se@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/08 11:26:36 by mmora-se          #+#    #+#             */
-/*   Updated: 2021/05/02 15:16:04 by mmora-se         ###   ########.fr       */
+/*   Updated: 2021/05/02 15:35:37 by mmora-se         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,6 +65,7 @@ void	ft_putnbr_fd(int n, int fd);
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **alst, t_list *new);
 int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
 int		ft_pow(int base, int exp);
 
 #endif
